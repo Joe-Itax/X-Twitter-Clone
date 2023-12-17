2fa521c4b2a8d00b78475c367552bb7e5788aa22
Joseph-Itakala <josephitakala18@gmail.com>
2023-12-17 13:53:22 +0000

    feat: make the "Trends" side

--- src/components/trends/trends.jsx
+++ src/components/trends/trends.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import "./trends.css"
 import Button from "../Buttons/button"
+// import Avatar from "../../assets/images/profile-photo.png"
+import Avatar from "../../assets/images/tweet-profile-photo-2.png"
+import Verified from "../../assets/icons/Verified.svg"
 
 function Trends() {
   const [isFocus, setIsFocus] = useState(false);
@@ -10,7 +14,10 @@ function Trends() {
   const onBlur = () => {
     setIsFocus(false);
   }
-  console.log("isFocus: ", isFocus);
+  const navigate = useNavigate();
+  const navigateToProfil = () => {
+    navigate("/profil")
+  }
   return (
     <>
       <div className="trends">
@@ -22,6 +29,7 @@ function Trends() {
             <input onFocus={onFocus} onBlur={onBlur} type="email" placeholder="Search" id="input" />
           </div>
         </div>
+
         <div className="trends-subs_premium bg__gray">
           <div className="trends-title">
             <span className="">Subscribe to Premium</span>
@@ -31,37 +39,129 @@ function Trends() {
           </div>
           <Button content="Subscribe" textColor="#fff" backgroundColor="#1d9bf0" />
         </div>
-        <div className="trends-subs_premium bg__gray">
-          <div className="trends-title">
-            <span className="">Subscribe to Premium</span>
+
+        <div className="trends__trends bg__gray">
+          <div className="trends__trends-title-box">
+            <div className="trends__trends-title">
+              <span>Trends</span>
+            </div>
+            <div className="trends__trends-icon_parameter">
+              <i class="ri-settings-5-line"></i>
+            </div>
           </div>
-          <div className="">
-            <span className="">Subscribe to unlock new features and if eligible, receive a share of ads revenue.</span>
+          <div className="trends__trends-content">
+            <div className="trends__trends-content-details-box">
+              <div className="trends__trends-content-details">
+                <span className="trending-localisation text-gray">Trending in Congo - Kinshasa</span>
+                <span className="trending-theme">Congo</span>
+                <span className="trending-posts-count text-gray">39.2K posts</span>
+              </div>
+              <div className="icon_more text-gray">
+                <i class="ri-more-line"></i>
+              </div>
+            </div>
           </div>
-          <Button content="Subscribe" textColor="#fff" backgroundColor="#1d9bf0" />
+          <div className="trends__trends-content">
+            <div className="trends__trends-content-details-box">
+              <div className="trends__trends-content-details">
+                <span className="trending-localisation text-gray">Trending in Congo - Kinshasa</span>
+                <span className="trending-theme">Congo</span>
+                <span className="trending-posts-count text-gray">39.2K posts</span>
+              </div>
+              <div className="icon_more text-gray">
+                <i class="ri-more-line"></i>
+              </div>
+            </div>
+          </div>
+          <div className="trends__trends-content">
+            <div className="trends__trends-content-details-box">
+              <div className="trends__trends-content-details">
+                <span className="trending-localisation text-gray">Trending in Congo - Kinshasa</span>
+                <span className="trending-theme">Congo</span>
+                <span className="trending-posts-count text-gray">39.2K posts</span>
+              </div>
+              <div className="icon_more text-gray">
+                <i class="ri-more-line"></i>
+              </div>
+            </div>
+          </div>
+          <Link to="/i/trends" className="show-more">Show more</Link>
         </div>
-        <div className="trends-subs_premium bg__gray">
-          <div className="trends-title">
-            <span className="">Subscribe to Premium</span>
+
+        <div className="trends__who-follow bg__gray">
+          <div className="trends__trends-title-box">
+            <div className="trends__trends-title">
+              <span>Who to follow</span>
+            </div>
           </div>
-          <div className="">
-            <span className="">Subscribe to unlock new features and if eligible, receive a share of ads revenue.</span>
+          <div className="trends__who-follow-content" onClick={navigateToProfil}>
+            <Link to="account" className="link-to-profil">
+              <div className="">
+                <img alt="Avatar" src={Avatar} />
+              </div>
+              <div className="">
+                <div className="title-profil">
+                  <span className="">Electronic Arts</span>
+                  <img alt="" src={Verified} style={{ width: "1rem" }} />
+                </div>
+                <span className="text-gray">@EA</span>
+              </div>
+            </Link>
+            <div>
+              <Button content="Follow" backgroundColor="#EFF4F4" textColor="#0F1419" />
+            </div>
           </div>
-          <Button content="Subscribe" textColor="#fff" backgroundColor="#1d9bf0" />
+          <div className="trends__who-follow-content" onClick={navigateToProfil}>
+            <Link to="account" className="link-to-profil">
+              <div className="">
+                <img alt="Avatar" src={Avatar} />
+              </div>
+              <div className="">
+                <div className="title-profil">
+                  <span className="">Electronic Arts</span>
+                  <img alt="" src={Verified} style={{ width: "1rem" }} />
+                </div>
+                <span className="text-gray">@EA</span>
+              </div>
+            </Link>
+            <div>
+              <Button content="Follow" backgroundColor="#EFF4F4" textColor="#0F1419" />
+            </div>
+          </div>
+          <div className="trends__who-follow-content" onClick={navigateToProfil}>
+            <Link to="account" className="link-to-profil">
+              <div className="">
+                <img alt="Avatar" src={Avatar} />
+              </div>
+              <div className="">
+                <div className="title-profil">
+                  <span className="">Electronic Arts</span>
+                  <img alt="" src={Verified} style={{ width: "1rem" }} />
+                </div>
+                <span className="text-gray">@EA</span>
+              </div>
+            </Link>
+            <div>
+              <Button content="Follow" backgroundColor="#EFF4F4" textColor="#0F1419" />
+            </div>
+          </div>
+          <Link to="/i/connect_people" className="show-more">Show more</Link>
         </div>
-        <div className="trends-subs_premium bg__gray">
-          <div className="trends-title">
-            <span className="">Subscribe to Premium</span>
-          </div>
-          <div className="">
-            <span className="">Subscribe to unlock new features and if eligible, receive a share of ads revenue.</span>
-          </div>
-          <Button content="Subscribe" textColor="#fff" backgroundColor="#1d9bf0" />
+
+        <div className="trends__trends-more">
+          <Link to="https://twitter.com/tos" target="_blank">Terms of Service</Link>
+          <Link to="https://twitter.com/privacy" target="_blank">Privacy Policy</Link>
+          <Link to="https://support.twitter.com/articles/20170514" target="_blank">Coockie Policy</Link>
+          <Link to="https://help.twitter.com/resources/accessibility" target="_blank">Accessibility</Link>
+          <Link to="https://business.twitter.com/en/help/troubleshooting/how-twitter-ads-work.html?ref=web-twc-ao-gbl-adsinfo&utm_source=twc&utm_medium=web&utm_campaign=ao&utm_content=adsinfo" target="_blank">Ads info</Link>
+          <span>More <i class="ri-more-line"></i></span>
+          <span className="copyright">© 2023 X Corp.</span>
         </div>
         {/* <h1>Trends</h1> */}
       </div>
     </>
   )
 }
+
 
 export default Trends;
